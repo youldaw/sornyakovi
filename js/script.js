c4eb6b972468f46ce7168dffa8457a2c806c4d8e
youldaw <68648932+youldaw@users.noreply.github.com>
2022-07-14 02:15:36 +0500

    products
    products brands

--- js/script.js
+++ js/script.js
@@ -32,5 +32,51 @@ $(function (){
         }
     });
 
+    $('.actual-slide').owlCarousel({
+        loop:true,
+        // autoplay : true,
+        // autoplayTimeout : 8000,
+        margin:0,
+        nav:false,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:2.5,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:2.5,
+            }
+        }
+    });
+
+    $('.brands-slide').owlCarousel({
+        loop:false,
+        // autoplay : true,
+        // autoplayTimeout : 8000,
+        margin:25,
+        nav:false,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:6,
+        responsive:{
+            0:{
+                items:2,
+            },
+            600:{
+                items:4,
+            },
+            1000:{
+                items:6,
+            }
+        }
+    });
+
 });
 
